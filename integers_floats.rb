c5acd5143f5370cf1800ab2e5882c1e691979269
yalasti <yvettecodes@gmail.com>
2015-03-05 06:48:44 -0500

    integers and floats

---
+++ integers_floats.rb
@@ -0,0 +1,18 @@
+#integers numbers
+
+puts 15 + 29
+puts 8 * 5
+puts 3 - 4
+puts 11 / 3   # answer rounded down
+
+
+
+
+
+
+# float numbers (decimal numbers)
+
+puts 6.0 + 8.9
+puts 3.5 * 3.0
+puts 7.0 - 11.0
+puts 13.0 / 3.0
